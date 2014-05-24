a20638676caf78be65967a1a6ad1f4360cbdce03
Ciro S. Costa <ciro.costa@usp.br>
2014-05-23 21:29:42 -0300

    thried try

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -14,10 +14,11 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['Chrome','Firefox'],
 
     plugins : [
             'karma-chrome-launcher',
+            'karma-firefox-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
             ],
